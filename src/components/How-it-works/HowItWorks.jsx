eb284009d612890c0d37abf056a01f7be1c3a4a2
aduspandu@gmail.com <aduspandu@gmail.com>
2024-09-08 03:23:26 +0530

    contact

--- src/components/How-it-works/HowItWorks.jsx
+++ src/components/How-it-works/HowItWorks.jsx
@@ -11,17 +11,27 @@ function HowItWorks() {
             <img className="base-image" src="public\assets\Group 2.png" alt="" />
             <div className='video-container'>
               <div className='video-1'>
-                <video className="video-element" controls>
-                  <source src="public\assets\earner creater.mp4" type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                <iframe 
+                  className="video-element"
+                  width="560" 
+                  height="315" 
+                  src="https://www.youtube.com/embed/7MDlK8DBtbo" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowFullScreen>
+                </iframe>
                 <img src="public\assets\For Creators.png" alt="" className='creator-image'/>
               </div>
               <div className='video-2'>
-                <video className="video-element" controls>
-                  <source src="public\assets\earnr hindi.mp4" type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                <iframe 
+                  className="video-element"
+                  width="560" 
+                  height="315" 
+                  src="https://www.youtube.com/embed/hXAC8lIgMr8" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowFullScreen>
+                </iframe>
                 <img src="public\assets\For Supporters.png" alt="" className='supporter-image'/>
               </div>
             </div>
